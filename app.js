9b748cecf37593afb67bb3e3fdc499682171b0f4
jsangalang93 <jsangalang93@gmail.com>
2024-03-28 15:16:39 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,123 @@
+/*
+Exercise 1: Define an empty array
+
+1) Create an empty array and assign it to a variable called `foods`.
+
+Exercise 1 has been completed for you:
+*/
+
+// const foods = [];  
+
+// console.log('Exercise 1 result:', foods);
+
+
+/*
+Exercise 2: Add strings to the array
+
+1) Add 'pizza' and 'cheeseburger' to the `foods` array. 
+
+Note: 'pizza' should be the first item in the array, followed by 'cheeseburger'.
+
+Complete Exercise 2 in the space below:
+*/
+
+
+
+//const foods = ['pizza', 'cheeseburger'];
+ //   console.log('Exercise 2 result:', foods);
+
+
+
+/*
+Exercise 3: Insert at the beginning
+
+1) Insert the string 'taco' at the beginning of the `foods` array.
+
+Complete Exercise 3 in the space below:
+*/
+
+
+
+// const foods = ['pizza', 'cheeseburger'];
+//     console.log(foods.unshift('taco'));
+    
+
+
+// console.log('Exercise 3 result:', foods);
+
+
+/*
+Exercise 4: Access an array element
+
+1) Retrieve the 'pizza' string from the array based on its position (index) in
+   the array.  
+
+2) Assign it to a variable called `favFood`.
+
+Complete Exercise 4 in the space below:
+*/
+
+
+
+// const foods = ['pizza', 'cheeseburger'];
+//     console.log(foods.unshift('taco'));
+//     const favFood = foods[1]
+
+
+// console.log('Exercise 4 result:', favFood);
+
+/*
+Exercise 5: Insert an element between two others
+
+1) Insert the string 'tofu' between 'pizza' and 'cheeseburger' in the array.
+
+Complete Exercise 5 in the space below:
+*/
+
+
+
+// const foods = ['pizza', 'cheeseburger'];
+//      console.log(foods.unshift('taco'));
+//      const favFood = foods[1]
+//      foods.splice(2, 0, 'tofu');
+
+
+// console.log('Exercise 5 result:', foods);
+
+
+
+/*
+Exercise 6: Replace elements
+
+1) Replace 'pizza' in the `foods` array with 'sushi' and 'cupcake'.
+
+Complete Exercise 6 in the space below:
+*/
+
+
+
+const foods = ['pizza', 'cheeseburger'];
+      console.log(foods.unshift('taco'));
+      const favFood = foods[1]
+      foods.splice(2, 0, 'tofu');
+      foods[1] = 'sushi'
+      foods.splice(2, 0, 'cupcake')
+      
+      console.log('Exercise 6 result:', foods);
+
+
+
+/*
+Exercise 7: Using the `slice()` method
+
+1) Use the `slice()` method to create a new array that contains 'sushi' and 
+   'cupcake'.
+
+2) Assign it to a variable named `yummy`.
+
+Complete Exercise 7 in the space below:
+*/
+
+
+
+console.log('Exercise 7 result:', yummy);
